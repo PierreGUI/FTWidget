7845aeff851d45682c803fbdb0301280bce46a08
PierreGUI <pierre.vdev@gmail.com>
2015-03-14 01:23:39 +0100

    Create example buttons

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,9 @@
-function doClick(e) {
-    alert($.label.text);
+function onClick(e) {
+    alert("Hello world!");
 }
+
+$.button0.construct($.createStyle({ id:"button0" }));
+$.button1.construct($.createStyle({ id:"button1" }));
+$.button2.construct($.createStyle({ id:"button2" }));
 
 $.index.open();
