1735cabbaa68df269fa278ce8f3c73604e304c77
PierreGUI <pierre.vdev@gmail.com>
2015-03-21 17:11:59 +0100

    Clean ProfileTile
    container rules are useless, config is automatically applied

--- app/widgets/ProfileTile/controllers/widget.js
+++ app/widgets/ProfileTile/controllers/widget.js
@@ -14,33 +14,10 @@ _.extend(this, {
         "name": "#name.text",
         "description": "#description.text",
         "image": "#avatar.image",
-        "backgroundColor": "#container.backgroundColor",
-        "borderColor": "#container.borderColor",
         "separatorColor": "#separator.backgroundColor",
-        "borderRadius": ["#container.borderRadius", "#avatar.borderRadius"],
-
+        "avatarRadius": "#avatar.borderRadius",
         "roundImage": function(boolean){
             return ["#avatar.borderRadius", boolean?25:0]; // RETURN ARRAY [rules, input]
-        },
-
-        // Apply style calculated from a fonction taking arg input
-        "tileType": {
-            "#border.backgroundColor": function(type){
-                var colors = {
-                    blue: "#2980b9",
-                    green: "#27ae60",
-                    red: "#c0392b"
-                };
-                return colors[type];
-            },
-            "#inner.backgroundColor": function(type){
-                var colors = {
-                    blue: "#3498db",
-                    green:"#2ecc71",
-                    red: "#e74c3c"
-                };
-                return colors[type];
-            },
         }
     },
 
